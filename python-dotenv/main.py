3e906c55a5cae14bce3176e954880a38463e9e5a
kita0623 <rippunaish312619@gmail.com>
2024-01-23 17:38:15 +0900

    modified

--- python-dotenv/main.py
+++ python-dotenv/main.py
@@ -1,6 +1,7 @@
 # .envファイルをロードする
 from dotenv import load_dotenv
 load_dotenv()
+# load_dotenv(verbose=True, dotenv_path='/path/to/.env') # .envのパスを指定する場合
 
 import os
 userName = os.getenv('USERNAME')
